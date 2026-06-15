65b5f552eb83f85688a1b6c6bc9f9874aebebb14
elbanhawysherif <elbanhawy@gmail.com>
2026-06-15 17:46:01 +0300

    Initialize Flask app with basic routes
    Added Flask app with routes for home and run-html.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, Response
+from screener import run_screener
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "PRO SIGNAL ENGINE RUNNING"
+
+
+@app.route("/run-html")
+def run_html():
+
+    data = run_screener()
+
+    return Response(
+        data["pretty_html"],
+        mimetype="text/html"
+    )
